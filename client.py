a0b00df5d0539c7c0595b9186232e5310b4a61a3
Kirin Harrington <94868417+MisterK123@users.noreply.github.com>
2023-01-20 10:46:07 +0000

    Update client.py

--- client.py
+++ client.py
@@ -1,28 +1,5 @@
 import socket, sys, threading, tkinter
 from tkinter import font
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try:
-    server.connect((sys.argv[1], int(sys.argv[2]))) # IP, PORT
-except:
-    server.connect((input("Enter IP: "), int(input("Enter Port: "))))
-players = []
-display_players = [["bob", 1]]
-try:
-    with open("leaderboard.txt", "r") as f:
-        players = f.readline().strip()
-except FileNotFoundError:
-    pass
-
-def handle():
-    global server, players, players_var
-    while True:
-        try:
-            name, score = server.recv(1024).decode("ascii").split("|")
-            players.append([name, score])
-            update_display()
-            server.send("Added player".encode("ascii"))
-        except Exception as e:
-            server.send(str(e).encode("ascii"))
 
 def update_display():
     global display_players, players, players_var
@@ -39,6 +16,27 @@ def update_display():
     players_var.set(display_players)
     print(display_players)
 
+
+def handle():
+    global server, players, players_var
+    while True:
+        try:
+            name, score = server.recv(1024).decode("ascii").split("|")
+            players.append([name, score])
+            update_display()
+            server.send("Added player".encode("ascii"))
+        except Exception as e:
+            server.send(str(e).encode("ascii"))
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    server.connect((sys.argv[1], int(sys.argv[2]))) # IP, PORT
+except:
+    server.connect((input("Enter IP: "), int(input("Enter Port: "))))
+players = []
+display_players = []
+
 handle_thread = threading.Thread(target=handle)
 handle_thread.daemon = True
 handle_thread.start()
@@ -54,4 +52,12 @@ players_var = tkinter.StringVar(value=display_players)
 tkinter.Label(root, text="Robotics Challenge Leaderboard", font=("Courier", 30), bg="#ffffff").pack(pady=15)
 board = tkinter.Listbox(root, listvariable=players_var, borderwidth=0, highlightthickness=0, font=("Courier", 22), width=root.winfo_screenwidth()-30, height=100)
 board.pack(padx=15, pady=15)
+
+try:
+    with open("leaderboard.txt", "r") as f:
+        exec(f"players = {f.readline().strip()}")
+        update_display()
+except FileNotFoundError:
+    pass
+
 root.mainloop()
